82bbc9f1a625bf01f115b42adf82807e8fd437d7
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2021-11-12 11:58:40 -0600

    Add service worker registration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,3 +11,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// register() enables the app to work offline and load faster.
+// Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// To undo this, change register() to unregister() below. 
+serviceWorkerRegistration.register();
