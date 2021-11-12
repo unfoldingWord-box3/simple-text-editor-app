31f4df52db1736511be1be6f50b3e2be22ed540a
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2021-11-11 18:41:25 -0600

    Update Layout.jsx
    Separating out upper and lower workspaces

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,22 +4,49 @@ import TextFileEditor from './TextFileEditor';
 function Layout() {
   const [sectionIndex, setSectionIndex] = useState(0);
 
+  const containerStyle = {
+    position: 'absolute',
+    left: '0',
+    right: '0',
+    height: '100%',
+    minHeight: '100%',
+  };
+
+  const upperStyle = {
+    top: '0',
+    height: '50%',
+    minHeight: '50%',  
+    overflowY: 'scroll',
+  };
+
+  const lowerStyle = {
+    bottom: '0',
+    height: '50%',
+    minHeight: '50%',
+    overflowY: 'scroll',
+  };
+
   const workspaceStyle = {
     width: '100%',
     display: 'flex',
     alignItems: 'flex-start',
   };
   return (
-    <div style={workspaceStyle}>
-      <div style={{ width: '100%', overflowY: 'scroll', padding: '0 1em', }}>
-        <TextFileEditor editable={false} sectionIndex={sectionIndex} onSectionIndex={setSectionIndex} />
+    <div style={containerStyle}>
+      <div style={upperStyle}>
+        <div style={workspaceStyle}>
+          <div style={{ width: '100%', overflowY: 'scroll', padding: '0 1em', }}>
+            <TextFileEditor editable={false} sectionIndex={sectionIndex} onSectionIndex={setSectionIndex} />
+          </div>
+          <div style={{ width: '100%', overflowY: 'scroll', padding: '0 1em', }}>
+            <TextFileEditor editable={false} sectionIndex={sectionIndex} onSectionIndex={setSectionIndex} />
+          </div>
+          <div style={{ width: '100%', overflowY: 'scroll', padding: '0 1em', }}>
+            <TextFileEditor sectionIndex={sectionIndex} onSectionIndex={setSectionIndex} />
+          </div>
+       </div>
       </div>
-      <div style={{ width: '100%', overflowY: 'scroll', padding: '0 1em', }}>
-        <TextFileEditor editable={false} sectionIndex={sectionIndex} onSectionIndex={setSectionIndex} />
-      </div>
-      <div style={{ width: '100%', overflowY: 'scroll', padding: '0 1em', }}>
-        <TextFileEditor sectionIndex={sectionIndex} onSectionIndex={setSectionIndex} />
-      </div>
+      <div style={lowerStyle}>Testing<br />more<br />rows<br />.<br />Testing<br />more<br />rows<br />.<br />Testing<br />more<br />rows<br />.<br />Testing<br />more<br />rows<br />.<br />Testing<br />more<br />rows<br />.<br />Testing<br />more<br />rows<br />.<br /></div>
     </div>
   );
 }
