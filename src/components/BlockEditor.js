ea835b4fa9fb6b42903a4e2717d0c8a8bcd034fa
Christopher Klapp <christopher@klapp.name>
2021-11-12 13:50:15 -0600

    make usfm markers bold

--- src/components/BlockEditor.js
+++ src/components/BlockEditor.js
@@ -1,10 +1,11 @@
+import { useMemo } from "react";
 
-export default function blockComponent ({ onVerse, ...props }) {
-  
+export default function BlockEditor ({ onVerse, text, dangerouslySetInnerHTML, ...props }) {
+
   const onBlockClick = (e) => {
     const regex = /\\v *(\d+)/;
-    const text = e.target.innerText;
-    const match = regex.exec(text);
+    const _text = e.target.innerText;
+    const match = regex.exec(_text);
     const verse = match && match[1];
     onVerse(verse);
   };
@@ -15,13 +16,23 @@ export default function blockComponent ({ onVerse, ...props }) {
     padding: '0 0.2em'
   };
 
+  const markerText = useMemo(() => {
+    const markerRegex = /(\\\w{1,2} *\d*)/g;
+    const replacer = "<strong>$1</strong>";
+    const _markerText = text.replace(/</g, '&lt;').replace(markerRegex, replacer);
+    return _markerText;
+  }, [text]);
+  
   return (
     <>
       <div
         {...props}
         onClick={onBlockClick}
         style={editorStyle}
-      />
+        suppressContentEditableWarning={true}
+        dangerouslySetInnerHTML={{ __html: markerText }}
+      >
+      </div>
       <hr />
     </>
   );
