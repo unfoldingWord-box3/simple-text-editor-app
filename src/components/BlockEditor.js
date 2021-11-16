263b10cacb740dd4e352d2722e9f45d04b5b8cd7
Christopher Klapp <christopher@klapp.name>
2021-11-16 12:39:35 -0500

    verse-span support in rendering and onVerseClick

--- src/components/BlockEditor.js
+++ src/components/BlockEditor.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function BlockEditor ({ onVerse, text, dangerouslySetInnerHTML, ...props }) {
 
   const onBlockClick = (e) => {
-    const regex = /\\v *(\d+)/;
+    const regex = /\\v *(\d+-?\d*)/;
     const _text = e.target.innerText;
     const match = regex.exec(_text);
     const verse = match && match[1];
@@ -19,7 +19,7 @@ export default function BlockEditor ({ onVerse, text, dangerouslySetInnerHTML, .
 
   const markerText = useMemo(() => {
     let _markerText = text.replace(/</g, '&lt;');
-    const markerRegex = /(\\[\w-]*\** *\d*)(?=[^:.])/g;
+    const markerRegex = /(\\[\w-]*\** *\d*-?\d*)(?=[^:.])/g;
     const replacer = "<strong>$1</strong>";
     _markerText = _markerText.replace(markerRegex, replacer);
     const attrRegex = /(x?-?[\w-]+=".*")/g;
