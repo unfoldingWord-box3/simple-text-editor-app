360ca4e4a3132b24cb053f9e11a403fe1cb60a80
blm <mcleanb@photo-nomad.net>
2023-02-22 13:36:14 -0500

    Working on making Windows install

---
+++ forge.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        background: './public/favicon-32x32.png',
+        format: 'ULFO',
+        name: `Simple USFM Alignment`
+      }
+    },
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        certificateFile: './cert.pfx',
+        certificatePassword: process.env.CERTIFICATE_PASSWORD,
+      },
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
